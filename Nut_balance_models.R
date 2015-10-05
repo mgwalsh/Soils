88fb5cade5222e48a2a7d2d0808587965561170e
mgwalsh <markusgwalsh@gmail.com>
2015-10-05 05:28:48 +0300

    archetype classification update

--- Nut_balance_models.R
+++ Nut_balance_models.R
@@ -1,4 +1,4 @@
-#' Multivariate soil nutrient balance benchmarks & archetype classification
+#' Multivariate soil nutrient benchmarks & archetype classification,
 #' with AfSIS-1 data: C,N and Mehlich-3 P,K,S,Ca & Mg from 60 sentinel sites
 #' M. Walsh, Oct. 2015
 
@@ -14,31 +14,43 @@ require(arm)
 dir.create("NB60_data", showWarnings=F)
 setwd("./NB60_data")
 
-# Data download
+# Download
 download("https://www.dropbox.com/s/wxmd9cx5m9h5b4r/Nutbal_60.csv.zip?dl=0", "Nutbal_60.csv.zip", mode="wb")
 unzip("Nutbal_60.csv.zip", overwrite=T)
 dat <- read.table("Nutbal_60.csv", header=T, sep=",")
 
-# Compositional data analysis setup
+# Compositional analysis setup
 vars <- c("Site","Lat","Lon","Depth","C","N","P","K","S","Ca","Mg")
 nb60 <- na.omit(dat[vars])
-nuts <- c("C","N","P","K","S","Ca","Mg")
+fpart <- c("C","N","P","K","S","Ca","Mg")
 nb60$Fv <- 1000000-rowSums(nb60[nuts]) ## calculates "fill value" (Fv), in mg/kg soil
 
 # Sequential binary partion & integrated log ratio (ilr) transform
-fpart <- c("C","N","P","K","S","Ca","Mg","Fv")
 cdata <- acomp(nb60[fpart])
-bpart <- t(matrix(c( 1, 1, 1, 1, 1, 1, 1,-1,
-                    -1,-1, 1, 1, 1, 1, 1, 0,
-                     0, 0, 1,-1, 1,-1,-1, 0,
-                     0, 0, 0, 1, 0,-1,-1, 0,
-                     0, 0, 1, 0,-1, 0, 0, 0,
-                     0, 0, 0, 0, 0, 1,-1, 0,
-                     1,-1, 0, 0, 0, 0, 0, 0), ncol=8, nrow=7, byrow=T))
+bpart <- t(matrix(c(-1,-1, 1, 1, 1, 1, 1,
+                     0, 0, 1,-1, 1,-1,-1,
+                     0, 0, 0, 1, 0,-1,-1,
+                     0, 0, 1, 0,-1, 0, 0,
+                     0, 0, 0, 0, 0, 1,-1,
+                     1,-1, 0, 0, 0, 0, 0), ncol=7, nrow=6, byrow=T))
 CoDaDendrogram(X=acomp(cdata), signary=bpart)  				
 idata <- as.data.frame(ilr(cdata, V=bpart))
-nb60.comp <- cbind(nb60, idata)
-write.csv(nb60.comp, "nb60_comp.csv", row.name = F)
+
+# Identify compositional archetypes ---------------------------------------
+set.seed(1235813)
+idata.arc <- stepArchetypes(data=idata, k=1:10, nrep=10, verbose=T)
+screeplot(idata.arc)
+
+# Select no. of archetypes (screeplot suggests k=7 archetypes)
+idata.arc7 <- bestModel(idata.arc[[7]])
+invarc <- ilrInv(parameters(idata.arc7), V=bpart)
+
+# Classify ilr's by "dominant archetypes" (DA's)
+arc <- predict(idata.arc7, idata)
+DA <- apply(arc, 1, which.max)
+nb60 <- cbind(nb60, idata, DA)
+write.csv(nb60, "nb60_comp.csv", row.names=F)
+
 
 
 
