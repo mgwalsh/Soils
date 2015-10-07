b3bf5473ff1e826bffbbacdca41b7586e2a3cf9a
mgwalsh <markusgwalsh@gmail.com>
2015-10-07 03:10:15 +0300

    REML model updates

--- Nut_balance_models.R
+++ Nut_balance_models.R
@@ -56,44 +56,44 @@ table(nb60$DA, nb60$Depth)
 
 # Site level summaries ----------------------------------------------------
 # V1 = ilr[P,K,S,Ca,Mg|C,N]
-V1.lmer <- lmer(V1~I(Depth/100)*factor(DA)+(1|Site), data=nb60)
+V1.lmer <- lmer(V1~I(Depth/100)+(1|Site), data=nb60)
 summary(V1.lmer)
 
 # Plot of site-level random effects and standard errors
 V1.ranef <- ranef(V1.lmer)
 V1.se <- se.coef(V1.lmer)
-coefplot(V1.ranef$Site[,1], V1.se$Site[,1], varnames=rownames(V1.ranef$Site), xlim=c(-6,6), CI=2, cex.var=0.6, cex.pts=0.9, main="ilr [P,K,S,Ca,Mg | C,N]")
+coefplot(V1.ranef$Site[,1], V1.se$Site[,1], varnames=rownames(V1.ranef$Site), xlim=c(-7,7), CI=2, cex.var=0.6, cex.pts=0.9, main="ilr [P,K,S,Ca,Mg | C,N]")
 
 # V2 = ilr[P,S|K,Ca,Mg]
-V2.lmer <- lmer(V2~I(Depth/100)*factor(DA)+(1|Site), data=nb60)
+V2.lmer <- lmer(V2~I(Depth/100)+(1|Site), data=nb60)
 summary(V2.lmer)
 V2.ranef <- ranef(V2.lmer)
 V2.se <- se.coef(V2.lmer)
-coefplot(V2.ranef$Site[,1], V2.se$Site[,1], varnames=rownames(V2.ranef$Site), xlim=c(-4,4), CI=2, cex.var=0.6, cex.pts=0.9, main="ilr [P,S | K,Ca,Mg]")
+coefplot(V2.ranef$Site[,1], V2.se$Site[,1], varnames=rownames(V2.ranef$Site), xlim=c(-5,5), CI=2, cex.var=0.6, cex.pts=0.9, main="ilr [P,S | K,Ca,Mg]")
 
 # V3 = ilr[K|Ca,Mg]
-V3.lmer <- lmer(V3~I(Depth/100)*factor(DA)+(1|Site), data=nb60)
+V3.lmer <- lmer(V3~I(Depth/100)+(1|Site), data=nb60)
 summary(V3.lmer)
 V3.ranef <- ranef(V3.lmer)
 V3.se <- se.coef(V3.lmer)
-coefplot(V3.ranef$Site[,1], V3.se$Site[,1], varnames=rownames(V3.ranef$Site), xlim=c(-2,2), CI=2, cex.var=0.6, cex.pts=0.9, main="ilr [K | Ca,Mg]")
+coefplot(V3.ranef$Site[,1], V3.se$Site[,1], varnames=rownames(V3.ranef$Site), xlim=c(-3,3), CI=2, cex.var=0.6, cex.pts=0.9, main="ilr [K | Ca,Mg]")
 
 # V4 = ilr[Ca|Mg]
-V4.lmer <- lmer(V4~I(Depth/100)*factor(DA)+(1|Site), data=nb60)
+V4.lmer <- lmer(V4~I(Depth/100)+(1|Site), data=nb60)
 summary(V4.lmer)
 V4.ranef <- ranef(V4.lmer)
 V4.se <- se.coef(V4.lmer)
 coefplot(V4.ranef$Site[,1], V4.se$Site[,1], varnames=rownames(V4.ranef$Site), xlim=c(-2,2), CI=2, cex.var=0.6, cex.pts=0.9, main="ilr [Ca | Mg]")
 
 # V5 = ilr[P|S]
-V5.lmer <- lmer(V5~I(Depth/100)*factor(DA)+(1|Site), data=nb60)
+V5.lmer <- lmer(V5~I(Depth/100)+(1|Site), data=nb60)
 summary(V5.lmer)
 V5.ranef <- ranef(V5.lmer)
 V5.se <- se.coef(V5.lmer)
-coefplot(V5.ranef$Site[,1], V5.se$Site[,1], varnames=rownames(V5.ranef$Site), xlim=c(-1,1), CI=2, cex.var=0.6, cex.pts=0.9, main="ilr [P | S]")
+coefplot(V5.ranef$Site[,1], V5.se$Site[,1], varnames=rownames(V5.ranef$Site), xlim=c(-2,2), CI=2, cex.var=0.6, cex.pts=0.9, main="ilr [P | S]")
 
 # V6 = ilr[C|N]
-V6.lmer <- lmer(V6~I(Depth/100)*factor(DA)+(1|Site), data=nb60)
+V6.lmer <- lmer(V6~I(Depth/100)+(1|Site), data=nb60)
 summary(V6.lmer)
 V6.ranef <- ranef(V6.lmer)
 V6.se <- se.coef(V6.lmer)
