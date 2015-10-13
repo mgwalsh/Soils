700584327c654f4b2ce25d13ea147f16cc8f096a
mgwalsh <markusgwalsh@gmail.com>
2015-10-13 03:54:10 +0300

    Changed ilr's

--- Nut_balance_setup.R
+++ Nut_balance_setup.R
@@ -27,12 +27,12 @@ cpart <- c("C","N","P","K","S","Ca","Mg","Fv")
 # Sequential binary partion & integrated log ratio (ilr) transform
 cdata <- acomp(nb60[cpart])
 bpart <- t(matrix(c( 1, 1, 1, 1, 1, 1, 1,-1,
-                    -1,-1, 1, 1, 1, 1, 1, 0,
-                     0, 0, 1,-1, 1,-1,-1, 0,
+                    -1, 1, 1, 1, 1, 1, 1, 0,
+                     0, 1, 1,-1, 1,-1,-1, 0,
                      0, 0, 0, 1, 0,-1,-1, 0,
-                     0, 0, 1, 0,-1, 0, 0, 0,
+                     0,-1,-1, 0, 1, 0, 0, 0,
                      0, 0, 0, 0, 0, 1,-1, 0,
-                     1,-1, 0, 0, 0, 0, 0, 0), ncol=8, nrow=7, byrow=T))
+                     0, 1,-1, 0, 0, 0, 0, 0), ncol=8, nrow=7, byrow=T))
 CoDaDendrogram(X=acomp(cdata), signary=bpart) ## mass balance mobile graph				
 idata <- as.data.frame(ilr(cdata, V=bpart))
 nb60 <- cbind(nb60, idata)
@@ -47,19 +47,19 @@ V1.ranef <- ranef(V1.lmer)
 V1.se <- se.coef(V1.lmer)
 coefplot(V1.ranef$Site[,1], V1.se$Site[,1], varnames=rownames(V1.ranef$Site), xlim=c(-3,3), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [C,N,P,K,S,Ca,Mg | Fv]")
 
-# V2 = ilr[P,K,S,Ca,Mg|C,N]
+# V2 = ilr[N,P,K,S,Ca,Mg|C]
 V2.lmer <- lmer(V2~I(Depth/100)+(1|Site), data=nb60)
 summary(V2.lmer)
 V2.ranef <- ranef(V2.lmer)
 V2.se <- se.coef(V2.lmer)
-coefplot(V2.ranef$Site[,1], V2.se$Site[,1], varnames=rownames(V2.ranef$Site), xlim=c(-7,7), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [P,K,S,Ca,Mg | C,N]")
+coefplot(V2.ranef$Site[,1], V2.se$Site[,1], varnames=rownames(V2.ranef$Site), xlim=c(-5,5), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [N,P,K,S,Ca,Mg | C]")
 
-# V3 = ilr[P,S|K,Ca,Mg]
+# V3 = ilr[N,P,S|K,Ca,Mg]
 V3.lmer <- lmer(V3~I(Depth/100)+(1|Site), data=nb60)
 summary(V3.lmer)
 V3.ranef <- ranef(V3.lmer)
 V3.se <- se.coef(V3.lmer)
-coefplot(V3.ranef$Site[,1], V3.se$Site[,1], varnames=rownames(V3.ranef$Site), xlim=c(-5,5), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [P,S | K,Ca,Mg]")
+coefplot(V3.ranef$Site[,1], V3.se$Site[,1], varnames=rownames(V3.ranef$Site), xlim=c(-6,6), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [N,P,S | K,Ca,Mg]")
 
 # V4 = ilr[K|Ca,Mg]
 V4.lmer <- lmer(V4~I(Depth/100)+(1|Site), data=nb60)
@@ -68,12 +68,12 @@ V4.ranef <- ranef(V4.lmer)
 V4.se <- se.coef(V4.lmer)
 coefplot(V4.ranef$Site[,1], V4.se$Site[,1], varnames=rownames(V4.ranef$Site), xlim=c(-3,3), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [K | Ca,Mg]")
 
-# V5 = ilr[P|S]
+# V5 = ilr[S|N,P]
 V5.lmer <- lmer(V5~I(Depth/100)+(1|Site), data=nb60)
 summary(V5.lmer)
 V5.ranef <- ranef(V5.lmer)
 V5.se <- se.coef(V5.lmer)
-coefplot(V5.ranef$Site[,1], V5.se$Site[,1], varnames=rownames(V5.ranef$Site), xlim=c(-2,2), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [P | S]")
+coefplot(V5.ranef$Site[,1], V5.se$Site[,1], varnames=rownames(V5.ranef$Site), xlim=c(-3,3), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [S | N,P]")
 
 # V6 = ilr[Ca|Mg]
 V6.lmer <- lmer(V6~I(Depth/100)+(1|Site), data=nb60)
@@ -82,15 +82,15 @@ V6.ranef <- ranef(V6.lmer)
 V6.se <- se.coef(V6.lmer)
 coefplot(V6.ranef$Site[,1], V6.se$Site[,1], varnames=rownames(V6.ranef$Site), xlim=c(-2,2), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [Ca | Mg]")
 
-# V7 = ilr[C|N]
+# V7 = ilr[N|P]
 V7.lmer <- lmer(V7~I(Depth/100)+(1|Site), data=nb60)
 summary(V7.lmer)
 V7.ranef <- ranef(V7.lmer)
 V7.se <- se.coef(V7.lmer)
-coefplot(V7.ranef$Site[,1], V7.se$Site[,1], varnames=rownames(V7.ranef$Site), xlim=c(-0.6,0.6), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [C | N]")
+coefplot(V7.ranef$Site[,1], V7.se$Site[,1], varnames=rownames(V7.ranef$Site), xlim=c(-3,3), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [N | P]")
 
 # Topsoil / subsoil contrast ----------------------------------------------
-tsc.glmer <- glmer(factor(Depth)~V1+V2+V4+V5+(1|Site), family="binomial"(link=logit), data=nb60)
+tsc.glmer <- glmer(factor(Depth)~V1+V2+V3+V4+V5+(1|Site), family="binomial"(link=logit), data=nb60)
 summary(tsc.glmer)
 tsc.ranef <- ranef(tsc.glmer)
 tsc.se <- se.coef(tsc.glmer)
@@ -99,7 +99,7 @@ fix <- fixef(tsc.glmer) ## extract mean effects
 
 # Proposed soil nutrient balance / fertility index (SFI), based on topsoil/subsoil contrast 
 attach(nb60)
-nb60$SFI <- (V1*fix[2]+V2*fix[3]+V4*fix[4]+V5*fix[5]+fix[1])*-1
+nb60$SFI <- (V1*fix[2]+V2*fix[3]+V3*fix[4]+V4*fix[5]+V5*fix[6]+fix[1])*-1
 detach(nb60)
 
 # Topsoil / subsoil contrast ecdf plot
