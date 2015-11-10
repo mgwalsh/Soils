9dff88c66d9f8b4b4bf8c9443757e62ce2e0e31c
mgwalsh <markusgwalsh@gmail.com>
2015-11-10 04:25:54 +0300

    Documentation update

--- Nut_balance_setup.R
+++ Nut_balance_setup.R
@@ -1,4 +1,4 @@
-#' Exploratory soil nutrient mass balances with AfSIS-1 data:
+#' Soil nutrient mass balances with AfSIS-1 data:
 #' C,N and Mehlich-3 extractable P,K,S,Ca & Mg, from 60 sentinel sites
 #' M. Walsh, Oct. 2015
 
@@ -16,8 +16,8 @@ setwd("./NB60_data")
 # Download
 download("https://www.dropbox.com/s/k9pti8a4fvaxjlm/Nutbal60.zip?dl=0", "Nutbal60.zip", mode="wb")
 unzip("Nutbal60.zip", overwrite=T)
-prof <- read.table("Profiles.csv", header=T, sep=",")
-samp <- read.table("Samples.csv", header=T, sep=",")
+prof <- read.table("Profiles.csv", header=T, sep=",") ## profile locations and site names
+samp <- read.table("Samples.csv", header=T, sep=",") ## sample data
 dat <- merge(prof, samp, by="PID")
 
 # Compositional analysis setup
@@ -39,58 +39,6 @@ bpart <- t(matrix(c( 1, 1, 1, 1, 1, 1, 1,-1,
 CoDaDendrogram(X=acomp(cdata), signary=bpart) ## mass balance mobile graph				
 idata <- as.data.frame(ilr(cdata, V=bpart))
 nb60 <- cbind(nb60, idata)
-
-# Site level summaries / expected values ----------------------------------
-# V1 = ilr[C,N,P,K,S,Ca,Mg|Fv]
-V1.lmer <- lmer(V1~I(Depth/100)+(1|Site), data=nb60)
-summary(V1.lmer)
-
-# Plot of site-level random effects and standard errors
-V1.ranef <- ranef(V1.lmer)
-V1.se <- se.coef(V1.lmer)
-coefplot(V1.ranef$Site[,1], V1.se$Site[,1], varnames=rownames(V1.ranef$Site), xlim=c(-3,3), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [C,N,P,K,S,Ca,Mg | Fv]")
-
-# V2 = ilr[P,K,S,Ca,Mg|C,N]
-V2.lmer <- lmer(V2~I(Depth/100)+(1|Site), data=nb60)
-summary(V2.lmer)
-V2.ranef <- ranef(V2.lmer)
-V2.se <- se.coef(V2.lmer)
-coefplot(V2.ranef$Site[,1], V2.se$Site[,1], varnames=rownames(V2.ranef$Site), xlim=c(-7,7), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [P,K,S,Ca,Mg | C,N]")
-
-# V3 = ilr[P,S|K,Ca,Mg]
-V3.lmer <- lmer(V3~I(Depth/100)+(1|Site), data=nb60)
-summary(V3.lmer)
-V3.ranef <- ranef(V3.lmer)
-V3.se <- se.coef(V3.lmer)
-coefplot(V3.ranef$Site[,1], V3.se$Site[,1], varnames=rownames(V3.ranef$Site), xlim=c(-5,5), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [P,S | K,Ca,Mg]")
-
-# V4 = ilr[K|Ca,Mg]
-V4.lmer <- lmer(V4~I(Depth/100)+(1|Site), data=nb60)
-summary(V4.lmer)
-V4.ranef <- ranef(V4.lmer)
-V4.se <- se.coef(V4.lmer)
-coefplot(V4.ranef$Site[,1], V4.se$Site[,1], varnames=rownames(V4.ranef$Site), xlim=c(-3,3), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [K | Ca,Mg]")
-
-# V5 = ilr[P|S]
-V5.lmer <- lmer(V5~I(Depth/100)+(1|Site), data=nb60)
-summary(V5.lmer)
-V5.ranef <- ranef(V5.lmer)
-V5.se <- se.coef(V5.lmer)
-coefplot(V5.ranef$Site[,1], V5.se$Site[,1], varnames=rownames(V5.ranef$Site), xlim=c(-3,3), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [P | S]")
-
-# V6 = ilr[Ca|Mg]
-V6.lmer <- lmer(V6~I(Depth/100)+(1|Site), data=nb60)
-summary(V6.lmer)
-V6.ranef <- ranef(V6.lmer)
-V6.se <- se.coef(V6.lmer)
-coefplot(V6.ranef$Site[,1], V6.se$Site[,1], varnames=rownames(V6.ranef$Site), xlim=c(-2,2), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [Ca | Mg]")
-
-# V7 = ilr[C|N]
-V7.lmer <- lmer(V7~I(Depth/100)+(1|Site), data=nb60)
-summary(V7.lmer)
-V7.ranef <- ranef(V7.lmer)
-V7.se <- se.coef(V7.lmer)
-coefplot(V7.ranef$Site[,1], V7.se$Site[,1], varnames=rownames(V7.ranef$Site), xlim=c(-0.6,0.6), CI=2, cex.var=0.6, cex.pts=1.0, main="ilr [C | N]")
 
 # Topsoil / subsoil contrast ----------------------------------------------
 tsc.glmer <- glmer(factor(Depth)~V1+V2+V4+V5+(1|Site), family="binomial"(link=logit), data=nb60)
