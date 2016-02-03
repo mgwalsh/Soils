16c9a2b2a68bef508c8da9f13d7ed1d8100b4c6a
mgwalsh <markusgwalsh@gmail.com>
2016-02-03 13:13:12 +0300

    added compositional balance

--- XRF_data_setup.R
+++ XRF_data_setup.R
@@ -1,13 +1,13 @@
-#' Geochemical enrichment/depletion factor model setup with AfSIS-1 XRF data:
+#' Geochemical weathering index model setup with AfSIS-1 XRF data
 #' XRF data from 60 sentinel sites
 #' M. Walsh, January 2016
 
-# install.packages(c("downloader","arm","MASS","colorRamps","RColorBrewer"), dependencies=T)
+# install.packages(c("downloader","MASS","colorRamps","RColorBrewer","compositions"), dependencies=T)
 require(downloader)
-require(arm)
 require(MASS)
 require(colorRamps)
 require(RColorBrewer)
+require(compositions)
 
 # Data setup --------------------------------------------------------------
 # Create a data folder in  your current working directory
@@ -30,24 +30,28 @@ k <- adjustcolor(brewer.pal(3, "Set1")[xdata$Depth], alpha=0.8)
 parcoord(xdata[,2:21], col = k)
 parcoord(xdata[,22:41], col = k)
 
-# Calculate weathering indices in equivalent oxide wt% units
+# Calculate equivalent oxide wt% units and weathering indices
 attach(xrfd)
+xrfd$Al2O3 <- (Al*1.8895)/10000
+xrfd$CaO   <- (Ca*1.2046)/10000
+xrfd$Na2O  <- (Na*1.3992)/10000
+xrfd$K2O   <- (K*1.3480)/10000
 xrfd$CIA <- (Al*1.8895)/(Al*1.8895+K*1.2046+(Ca*1.3992-(P*2.2916*10/3))+Na*1.3480)*100
 xrfd$CIW <- (Al*1.8895)/(Al*1.8895+(Ca*1.3992-(P*2.2916*10/3))+Na*1.3480)*100
 detach(xrfd)
 plot(CIW ~ CIA, data=xrfd, xlim=c(0,100), ylim=c(0,100))
 abline(c(0,1), lwd=2, col="red")
 
-# Site level summaries ----------------------------------------------------
-CIA.lmer <- lmer(CIA~I(Depth/100)+(1|Site), data=xrfd)
-summary(CIA.lmer)
-CIA.ranef <- ranef(CIA.lmer)
-CIA.se <- se.coef(CIA.lmer)
-coefplot(CIA.ranef$Site[,1], CIA.se$Site[,1], varnames=rownames(CIA.ranef$Site), xlim=c(-60,60), CI=2, cex.var=0.6, cex.pts=1.0, main="")
+# Compositional analysis setup
+vars <- c("SSN","Site","Lat","Lon","Depth","Al2O3","CaO","Na2O","K2O","CIA","CIW")
+wi60 <- na.omit(xrfd[vars])
+cpart <- c("Al2O3","CaO","Na2O","K2O")
 
-CIW.lmer <- lmer(CIW~I(Depth/100)+(1|Site), data=xrfd)
-summary(CIW.lmer)
-CIW.ranef <- ranef(CIW.lmer)
-CIW.se <- se.coef(CIW.lmer)
-coefplot(CIW.ranef$Site[,1], CIW.se$Site[,1], varnames=rownames(CIW.ranef$Site), xlim=c(-60,60), CI=2, cex.var=0.6, cex.pts=1.0, main="")
-
+# Sequential binary partion & isometric log ratio (ilr) transform
+cdata <- acomp(wi60[cpart])
+bpart <- t(matrix(c(1,-1,-1,-1,
+                    0,-1,-1, 1,
+                    0, 1,-1, 0), ncol=4, nrow=3, byrow=T))
+CoDaDendrogram(X=acomp(cdata), signary=bpart, type="lines") ## mass balance mobile graph				
+idata <- as.data.frame(ilr(cdata, V=bpart))
+wi60 <- cbind(wi60, idata)
