58a26cc3ec7f181d2e0e15a254f2ad201c378cac
mgwalsh <markusgwalsh@gmail.com>
2016-03-20 13:20:48 +0300

    Soil nutrient mass balance benchmarks

--- Nut_balances.R
+++ Nut_balances.R
@@ -2,11 +2,10 @@
 #' C,N, Mehlich-3 extractable elements, XRF A-CN-K data and Laser Diffraction data from 60 sentinel sites
 #' M. Walsh, January 2016
 
-# install.packages(c("devtools","downloader","compositions","arm","quantreg","circlize","RColorBrewer"), dependencies=T)
+# install.packages(c("devtools","downloader","arm","quantreg","circlize","RColorBrewer"), dependencies=T)
 suppressPackageStartupMessages({
 require(devtools)
 require(downloader)
-require(compositions)
 require(arm)
 require(quantreg)
 require(circlize)
@@ -15,8 +14,8 @@ require(RColorBrewer)
 
 # Data setup --------------------------------------------------------------
 # Run this first
-SourceURL <- "https://raw.githubusercontent.com/mgwalsh/Soils/master/Nut_balance_setup.R"
-source_url(SourceURL)
+# SourceURL <- "https://raw.githubusercontent.com/mgwalsh/Soils/master/Nut_balance_setup.R"
+# source_url(SourceURL)
 
 # load Mehlich-3 Al,B,Cu,Fe,Mn & Zn data
 download("https://www.dropbox.com/s/bivkvxrjno8fo67/nb60_micro.csv?dl=0", "nb60_micro.csv", mode="wb")
@@ -39,23 +38,14 @@ nb60 <- merge(nb60, ldsp, by="SSN")
 # Plot chord diagram of affinities ------------------------------------------
 # Extract variables
 vars <- c("Fv","C","N","P","K","Ca","Mg","S")
-varl <- c("C","N","P","K","Ca","Mg","S","Mn","B","Cu","Zn","Fe")
 mnus <- nb60[vars]
-mnul <- nb60[varl]
 
 # Calculate compositional affinities
-mdats <- as.data.frame(clr(acomp(mnus)))
-mcors <- (cor(mnus))^2 ## affinities = sum of compositional coefficients of determination (R^2)
+mcors <- (cor(mnus))^2 ## affinities = sum of compositional coefficients of determination
 mcors[lower.tri(mcors, diag=F)] <- 0
 mcors <- ifelse(mcors < 0.05 | mcors == 1.0, 0, mcors)
 
-mdatl <- as.data.frame(clr(acomp(mnul)))
-mcorl <- (cor(mdatl))^2 ## affinities = sum of compositional coefficients of determination (R^2)
-mcorl[lower.tri(mcorl, diag=F)] <- 0
-mcorl <- ifelse(mcorl < 0.1 | mcorl == 1.0, 0, mcorl)
-
-# Chord diagrams
-# Macro nutrient affinities
+# Chord diagram
 set.seed(1235813)
 circos.par(gap.degree = 3)
 chordDiagram(mcors, directional = F, annotationTrack = "grid",
@@ -72,30 +62,14 @@ circos.trackPlotRegion(track.index = 2, panel.fun = function(x, y) {
 }, bg.border = NA)
 circos.clear()
 
-# Macro+micro nutrient affinities
-set.seed(123)
-circos.par(gap.degree = 3)
-chordDiagram(mcorl, directional = F, annotationTrack = "grid",
-             preAllocateTracks = list(list(track.height = 0.05),
-                                      list(track.height = 0.05)))
-circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
-  xlim = get.cell.meta.data("xlim")
-  ylim = get.cell.meta.data("ylim")
-  sector.index = get.cell.meta.data("sector.index")
-  circos.text(mean(xlim), mean(ylim), sector.index, facing = "clockwise", niceFacing = T)
-}, bg.border = NA)
-circos.trackPlotRegion(track.index = 2, panel.fun = function(x, y) {
-  circos.axis("bottom", major.tick.percentage = 0.2, labels.cex = 0.6)
-}, bg.border = NA)
-circos.clear()
-
 # Quantile enrichment/ depletion factor (EDF) models ----------------------
 # Data setup
-edf <- cbind(nb60[c(1,5,8,17:25,36:37,41,45:46)], mdats)
+vars <- c("SSN","Site","Depth","CP","wV1","tV1","N","P","K","V1","V2","V3") 
+edfs <- nb60[vars]
 tauQL <- 0.5 ## set dependent variable quantile reference level
 
 # clr [N] | Depth, GeoSurvey, A-CN-K, LDPSA
-N.rq <- rq(N~log(Depth)+CP+wV1*tV1, tau = tauQL, data=edf) 
+N.rq <- rq(N~log(Depth)+CP+wV1*tV1, tau = tauQL, data=edfs) 
 summary(N.rq, se="boot", bsmethod="xy")
 
 # N-quantile regression coefficients plot
@@ -105,7 +79,7 @@ names <- c("log(Depth)","Cropland","CWI","PWI","CWI*PWI")
 coefplot(est, ses, varnames=names, cex.var=1, cex.pts=1.5, col.pts="blue", CI=2, xlim=c(-0.6,0.6), main="", mar=c(0.2,4,3,1))
 
 # clr [P] | Depth, GeoSurvey, A-CN-K, LDPSA
-P.rq <- rq(P~log(Depth)+CP+wV1*tV1, tau = tauQL, data=edf) 
+P.rq <- rq(P~log(Depth)+CP+wV1*tV1, tau = tauQL, data=edfs) 
 summary(P.rq, se="boot", bsmethod="xy")
 
 # P-quantile regression coefficients plot
@@ -114,7 +88,7 @@ ses <- as.vector(coef(summary(P.rq, se="boot", bsmethod="xy"))[2:6,2])
 coefplot(est, ses, cex.pts=1.5, offset=0.1, CI=2, main="", add=T)
 
 # clr [K] | Depth, GeoSurvey, A-CN-K, LDPSA
-K.rq <- rq(K~log(Depth)+CP+wV1*tV1, tau = tauQL, data=edf) 
+K.rq <- rq(K~log(Depth)+CP+wV1*tV1, tau = tauQL, data=edfs) 
 summary(K.rq, se="boot", bsmethod="xy")
 
 # K-quantile regression coefficients plot
@@ -123,7 +97,7 @@ ses <- as.vector(coef(summary(K.rq, se="boot", bsmethod="xy"))[2:6,2])
 coefplot(est, ses, cex.pts=1.5, col.pts="red", offset=0.2, CI=2, main="", add=T)
 
 # V1 = ilr [C,N,P,K,Ca,Mg,S | Fv] | Depth, GeoSurvey, A-CN-K, LDPSA
-V1.rq <- rq(V1~log(Depth)+CP+wV1*tV1, tau = tauQL, data=edf) 
+V1.rq <- rq(V1~log(Depth)+CP+wV1*tV1, tau = tauQL, data=edfs) 
 summary(V1.rq, se="boot", bsmethod="xy")
 
 # V1-quantile regression coefficient plot
@@ -133,7 +107,7 @@ names <- c("log(Depth)","Cropland","CWI","PWI","CWI*PWI")
 coefplot(est, ses, varnames=names, cex.var=1, cex.pts=1.5, col.pts="blue", CI=2, xlim=c(-1.5,1.5), main="", mar=c(0.2,4,3,1))
 
 # V2 = ilr [P,K,Ca,Mg,S | C,N] | Depth, GeoSurvey, A-CN-K, LDPSA
-V2.rq <- rq(V2~log(Depth)+CP+wV1*tV1, tau = tauQL, data=edf) 
+V2.rq <- rq(V2~log(Depth)+CP+wV1*tV1, tau = tauQL, data=edfs) 
 summary(V2.rq, se="boot", bsmethod="xy")
 
 # V2-quantile regression coefficients plot
@@ -142,7 +116,7 @@ ses <- as.vector(coef(summary(V2.rq, se="boot", bsmethod="xy"))[2:6,2])
 coefplot(est, ses, cex.pts=1.5, offset=0.1, CI=2, main="", add=T)
 
 # V3 = ilr [P,S | K,Ca,Mg] | Depth, GeoSurvey, A-CN-K, LDPSA
-V3.rq <- rq(V3~log(Depth)+CP+wV1*tV1, tau = tauQL, data=edf) 
+V3.rq <- rq(V3~log(Depth)+CP+wV1*tV1, tau = tauQL, data=edfs) 
 summary(V3.rq, se="boot", bsmethod="xy")
 
 # V3-quantile regression coefficients plot
@@ -152,8 +126,8 @@ coefplot(est, ses, cex.pts=1.5, col.pts="red", offset=0.2, CI=2, main="", add=T)
 
 # Site-level enrichment-depletion factors (EDFs) --------------------------
 # N EDF REML estimates
-edf$NEDF <- predict(N.rq, edf)
-N.lmer <- lmer(I(N-NEDF)~1+(1|Site), edf)
+edfs$NEDF <- predict(N.rq, edfs)
+N.lmer <- lmer(I(N-NEDF)~1+(1|Site), edfs)
 summary(N.lmer)
 ran <- ranef(N.lmer)
 ses <- se.coef(N.lmer)
@@ -161,8 +135,8 @@ nam <- rownames(ran$Site)
 coefplot(ran$Site[31:60,1], ses$Site[31:60,1], varnames=nam[31:60], cex.var=0.8, col.pts="blue", xlim=c(-1.5,1.5), CI=2, main="")
 
 # P EDF REML estimates
-edf$PEDF <- predict(P.rq, edf)
-P.lmer <- lmer(I(P-PEDF)~1+(1|Site), edf)
+edfs$PEDF <- predict(P.rq, edfs)
+P.lmer <- lmer(I(P-PEDF)~1+(1|Site), edfs)
 summary(P.lmer)
 ran <- ranef(P.lmer)
 ses <- se.coef(P.lmer)
@@ -170,8 +144,8 @@ nam <- rownames(ran$Site)
 coefplot(ran$Site[31:60,1], ses$Site[31:60,1], offset=0.2, CI=2, main="", add=T)
 
 # K EDF REML estimates
-edf$KEDF <- predict(K.rq, edf)
-K.lmer <- lmer(I(K-KEDF)~1+(1|Site), edf)
+edfs$KEDF <- predict(K.rq, edfs)
+K.lmer <- lmer(I(K-KEDF)~1+(1|Site), edfs)
 summary(K.lmer)
 ran <- ranef(K.lmer)
 ses <- se.coef(K.lmer)
