8076b15ab2a226cc1710591dd31805ded71c59f5
mgwalsh <markusgwalsh@gmail.com>
2019-09-28 17:19:32 +0300

    Alpha ZnSe spectrometer recalibration data setup

--- Alpha_recal_data.R
+++ Alpha_recal_data.R
@@ -19,5 +19,11 @@ setwd("./alpha_data")
 # Download
 download("https://osf.io/ut7ya?raw=1", "alpha_ref_data.zip", mode="wb")
 unzip("alpha_ref_data.zip", overwrite=T)
-wet <- read.table("wet.csv", header=T, sep=",") ## pH, EC, Hp, CN & M3 data
+wet <- read.table("wet.csv", header=T, sep=",") ## pH, EC, Hp, C, N & M3 data
 alpha <- read.table("alpha.csv", header=T, sep=",") ## alpha ZnSe spectral data
+
+# Compositional data analysis setup ---------------------------------------
+vars <- c("C","N","P","S","K","Ca","Mg","Na","Fe","Mn","Zn","Cu")
+nbal <- na.omit(wet[vars])
+nbal$Fv <- 1000000-rowSums(nbal[vars]) ## calculates "fill value" (Fv), in mg/kg soil
+
