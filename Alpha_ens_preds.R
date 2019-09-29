1b00b8a6a65d36c808b0be0436fa286d25cd44cf
mgwalsh <markusgwalsh@gmail.com>
2019-09-29 15:49:16 +0300

    Stacked predictions of soil properties with Alpha ZnSe spectra

--- Alpha_ens_preds.R
+++ Alpha_ens_preds.R
@@ -17,7 +17,7 @@ suppressPackageStartupMessages({
 # Data setup --------------------------------------------------------------
 # Run this first: https://github.com/mgwalsh/Cropland-Atlas/blob/master/ZM_GS19_data.R
 rm(list=setdiff(ls(), c("nbal"))) ## scrubs extraneous objects in memory)
-nbal <- as.data.frame(nbal[complete.cases(nbal[ ,c(1:1727)]),]) ## removes incomplete cases
+# nbal <- as.data.frame(nbal[complete.cases(nbal[ ,c(1:1727)]),]) ## removes incomplete cases
 
 # set calibration/validation set randomization seed
 seed <- 12358
@@ -56,6 +56,9 @@ fname <- paste("./Results/", labs, "_pls.rds", sep = "")
 saveRDS(pls, fname)
 
 # Random forest <randomForest> --------------------------------------------
+seed <- 12358
+set.seed(seed)
+
 # start doParallel to parallelize model fitting
 mc <- makeCluster(detectCores())
 registerDoParallel(mc)
@@ -66,14 +69,65 @@ tc <- trainControl(method="cv", allowParallel=T)
 tg <- expand.grid(mtry = seq(10,100, by=10)) ## model tuning steps
 
 # model training
-rf <- train(fcal, lcal,
-            preProc = c("center","scale"),
-            method = "rf",
-            ntree = 501,
-            tuneGrid = tg,
-            trControl = tc)
-print(rf)
+rf1 <- train(fcal, lcal,
+             preProc = c("center","scale"),
+             method = "rf",
+             ntree = 501,
+             tuneGrid = tg,
+             trControl = tc)
+print(rf1)
 stopCluster(mc)
-fname <- paste("./Results/", labs, "_rf.rds", sep = "")
+fname <- paste("./Results/", labs, "_rf1.rds", sep = "")
+saveRDS(rf1, fname)
+
+# Random forest with spectral PCA covariates
+seed <- 12358
+set.seed(seed)
+
+# PCA features
+fpca <- cal[,1728:1747]
+
+# start doParallel to parallelize model fitting
+mc <- makeCluster(detectCores())
+registerDoParallel(mc)
+
+# control setup
+set.seed(1385321)
+tc <- trainControl(method="cv", allowParallel=T)
+tg <- expand.grid(mtry = seq(2,20, by=1)) ## model tuning steps
+
+# model training
+rf2 <- train(fpca, lcal,
+             preProc = c("center","scale"),
+             method = "rf",
+             ntree = 501,
+             tuneGrid = tg,
+             trControl = tc)
+print(rf2)
+stopCluster(mc)
+fname <- paste("./Results/", labs, "_rf2.rds", sep = "")
 saveRDS(rf, fname)
 
+# Generalized boosting <gbm> ----------------------------------------------
+seed <- 12358
+set.seed(seed)
+
+# start doParallel to parallelize model fitting
+mc <- makeCluster(detectCores())
+registerDoParallel(mc)
+
+# control setup
+set.seed(1385321)
+tc <- trainControl(method = "cv", allowParallel = T)
+tg <- expand.grid(interaction.depth = seq(10,100, by=10), shrinkage = seq(0.02,0.1, by=0.02), n.trees = 501,
+                  n.minobsinnode = 25) ## model tuning steps
+
+gb <- train(fcal, lcal, 
+            method = "gbm", 
+            preProc = c("center", "scale"),
+            trControl = tc,
+            tuneGrid = tg)
+print(gb)
+stopCluster(mc)
+fname <- paste("./Results/", labs, "_gb.rds", sep = "")
+saveRDS(gb, fname)
