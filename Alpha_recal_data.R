8e4b5f9bae5169868214f620bc3bda7ac90fcdc1
mgwalsh <markusgwalsh@gmail.com>
2019-09-29 04:10:58 +0300

    Alpha ZnSe spectrometer recalibration data setup

--- Alpha_recal_data.R
+++ Alpha_recal_data.R
@@ -7,7 +7,6 @@ suppressPackageStartupMessages({
   require(downloader)
   require(compositions)
   require(archetypes)
-  require(RColorBrewer)
 })
 rm(list = ls())
 
@@ -20,23 +19,22 @@ setwd("./alpha_data")
 download("https://osf.io/ut7ya?raw=1", "alpha_ref_data.zip", mode="wb")
 unzip("alpha_ref_data.zip", overwrite=T)
 wet <- read.table("wet.csv", header=T, sep=",") ## pH, EC, Hp, C, N & M3 data
+vars <- c("SSID","C","N","P","K","S","Ca","Mg","Na","Fe","Mn","Cu","Zn")
+wet <- na.omit(wet[vars])
 alpha <- read.table("alpha.csv", header=T, sep=",") ## alpha ZnSe spectral data
 
 # Compositional data analysis setup ---------------------------------------
-vars <- c("C","N","P","K","S","Ca","Mg","Na")
-nbal <- na.omit(wet[vars])
-nbal$Fv <- 1000000-rowSums(nbal[vars]) ## calculates "fill value" (Fv), in mg/kg soil
+vars <- c("C","N","P","K","S","Ca","Mg","Na","Fe","Mn","Cu","Zn")
+wet$Fv <- 1000000-rowSums(wet[vars]) ## calculates "fill value" (Fv), in mg/kg soil
 
-# isometric log ratio transform
-vars <- c("C","N","P","K","S","Ca","Mg","Na","Fv") ## all values in mg/kg 
-cdat <- acomp(nbal[vars])
-bpart <- t(matrix(c( 1, 1, 1, 1, 1, 1, 1, 1,-1,
-                    -1,-1, 1, 1, 1, 1, 1, 1, 0,
-                     0, 0, 1,-1, 1,-1,-1,-1, 0,
-                     0, 0, 0,-1, 0,-1,-1, 1, 0,
-                     0, 0, 0, 1, 0,-1,-1, 0, 0, 
-                     0, 0, 1, 0,-1, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0, 1,-1, 0, 0,
-                     1,-1, 0, 0, 0, 0, 0, 0, 0), ncol=9, nrow=8, byrow=T))
-CoDaDendrogram(X=acomp(cdat), signary=bpart, type="lines") ## mass balance mobile graph				
-idat <- as.data.frame(ilr(cdat, V=bpart)) ## isometric log ratio (ilr) transform
+# Centered log ratio (clr) transform
+vars <- c("C","N","P","K","S","Ca","Mg","Na","Fe","Mn","Cu","Zn","Fv")
+nbal <- wet[vars]
+nbal <- as.data.frame(clr(nbal)) ## centered log ratio (clr) transform
+nbal <- cbind(wet$SSID, nbal)
+colnames(nbal)[colnames(nbal)=="wet$SSID"] <- "SSID"
+
+# merge with spectral data
+nbal <- merge(nbal, alpha, by="SSID")
+write.csv(nbal, "nbal_2019.csv", row.names=F)
+
