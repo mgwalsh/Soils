3f4e1e62722b173dd27c911b027d18f7e23193df
mgwalsh <markusgwalsh@gmail.com>
2019-09-30 02:57:44 +0300

    Alpha ZnSe spectrometer recalibration data setup

--- Alpha_recal_data.R
+++ Alpha_recal_data.R
@@ -19,16 +19,16 @@ setwd("./alpha_data")
 download("https://osf.io/ut7ya?raw=1", "alpha_ref_data.zip", mode="wb")
 unzip("alpha_ref_data.zip", overwrite=T)
 wet <- read.table("wet.csv", header=T, sep=",") ## pH, EC, Hp, C, N & M3 data
-vars <- c("SSID","C","N","P","K","S","Ca","Mg","Na","Fe","Mn","Zn")
+vars <- c("SSID","C","N","P","K","S","Ca","Mg","Na","Fe","Mn","Cu","Zn")
 wet <- na.omit(wet[vars])
 alpha <- read.table("alpha.csv", header=T, sep=",") ## alpha ZnSe spectral data
 
 # Compositional data analysis setup ---------------------------------------
-vars <- c("C","N","P","K","S","Ca","Mg","Na","Fe","Mn","Zn")
+vars <- c("C","N","P","K","S","Ca","Mg","Na","Fe","Mn","Cu","Zn")
 wet$Fv <- 1000000-rowSums(wet[vars]) ## calculates "fill value" (Fv), in mg/kg soil
 
 # Centered log ratio (clr) transform
-vars <- c("C","N","P","K","S","Ca","Mg","Na","Fe","Mn","Zn","Fv")
+vars <- c("C","N","P","K","S","Ca","Mg","Na","Fe","Mn","Cu","Zn","Fv")
 nbal <- wet[vars]
 nbal <- as.data.frame(clr(nbal)) ## centered log ratio (clr) transform
 nbal <- cbind(wet$SSID, nbal)
