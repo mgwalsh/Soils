e3efb2f707ddb9dc32b0cb6f7b40ea4adbd16251
mgwalsh <markusgwalsh@gmail.com>
2019-10-22 02:40:16 +0300

    Alpha-MIR scoring rules for soil remediation decisions

---
+++ Alpha_score_model.R
@@ -0,0 +1,26 @@
+# Alpha-MIR scoring rules for soil remediation decisions
+# M. Walsh, October 2019
+
+# install.packages(c("downloader","arm"), dependencies=T)
+suppressPackageStartupMessages({
+  require(downloader)
+  require(arm)
+})
+rm(list = ls())
+
+# Data setup --------------------------------------------------------------
+# Create a data folder in your current working directory
+dir.create("alpha_data", showWarnings=F)
+setwd("./alpha_data")
+
+# Download
+download("https://osf.io/wyrup?raw=1", "alpha_long_preds.csv.zip", mode="wb")
+unzip("alpha_long_preds.csv.zip", overwrite=T)
+rals <- read.table("alpha_long_preds.csv", header=T, sep=",") ## RAL and MIR predictions
+
+# Scoring rules -----------------------------------------------------------
+c50 <- glmer(c50~mir+(mir|cpart)+(1|sample), family=binomial(link="logit"), data=rals)
+summary(c50)
+rals$score <- fitted(c50)
+
+
