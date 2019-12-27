c92a3647f62e73310e3f96f460e58c2cd482825a
mgwalsh <markusgwalsh@gmail.com>
2019-12-27 14:45:26 -0700

    Alpha ZnSe spectrometer recalibration data setup

--- Alpha_recal_data.R
+++ Alpha_recal_data.R
@@ -13,11 +13,14 @@ rm(list = ls())
 # Create a data folder in your current working directory
 dir.create("alpha_data", showWarnings=F)
 setwd("./alpha_data")
+dir.create("Results", showWarnings = F)
 
 # Download
 download("https://osf.io/ut7ya?raw=1", "alpha_ref_data.zip", mode="wb")
 unzip("alpha_ref_data.zip", overwrite=T)
 wet <- read.table("wet.csv", header=T, sep=",") ## pH, EC, Hp, C, N & M3 data
+vars <- c("SSID","pH","Hp","Ca","Mg")
+cec <- na.omit(wet[vars])
 vars <- c("SSID","C","N","P","K","S","Ca","Mg","Na","Fe","Mn","Cu","Zn")
 wet <- na.omit(wet[vars])
 alpha <- read.table("alpha.csv", header=T, sep=",") ## Alpha ZnSe spectral data
@@ -41,7 +44,7 @@ pcas <- pcas[,1:20]
 
 # Merge & write files -----------------------------------------------------
 alpha <- cbind(alpha, pcas)
+cec <- merge(cec, alpha, by="SSID")
 nbal <- merge(nbal, alpha, by="SSID")
-dir.create("Results", showWarnings = F)
 write.csv(nbal, "./Results/nbal_2019.csv", row.names=F)
 
