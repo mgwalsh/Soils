3cb61a3cae0bdca3a4f0f45f98cae5d561a96a5d
mgwalsh <markusgwalsh@gmail.com>
2020-07-08 05:50:32 +0300

    Africa-wide SOC data setup

--- AF_SOC_data.R
+++ AF_SOC_data.R
@@ -17,7 +17,7 @@ dir.create("AF_SOC", showWarnings=F)
 setwd("./AF_SOC")
 
 # download SOC profile data (courtesy ISRIC)
-download("https://osf.io/df7hg/raw=1", "socsat.zip", mode="wb")
+download("https://osf.io/df7hg?raw=1", "socsat.zip", mode="wb")
 unzip("socsat.zip", overwrite=T)
 prof <- read.table("Profiles.csv", header=T, sep=",") ## profile locations & collection year
 prof <- prof[complete.cases(prof[ ,3:4]),] ## delete non-georeferenced profiles
@@ -32,7 +32,7 @@ plot(grids, axes = FALSE)
 
 # Data setup ---------------------------------------------------------------
 # project SOC coords to grid CRS
-prof.proj <- as.data.frame(project(cbind(prof$Lon, prof$Lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
+prof.proj <- as.data.frame(project(cbind(prof$lon, prof$lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
 colnames(prof.proj) <- c("x","y")
 prof <- cbind(prof, prof.proj)
 coordinates(prof) <- ~x+y
@@ -41,8 +41,8 @@ projection(prof) <- projection(grids)
 # extract gridded variables at profile locations
 socgrid <- extract(grids, prof)
 prof <- as.data.frame(cbind(prof, socgrid))
-soc <- merge(prof, samp, by="PID") ## merge samples by profile ID (PID)
-socc <- soc[complete.cases(soc[ ,c(16,18:19)]),] ## delete cases with incomplete Sand, pH or SOC measurements
+soc <- merge(prof, samp, by="pid") ## merge samples by profile ID (pid)
+# socc <- soc[complete.cases(soc[ ,c(17,19:20)]),] ## delete cases with incomplete Sand, pH or SOC measurements
 
 # Write file --------------------------------------------------------------
 write.csv(soc, "socdat.csv", row.names = F)
@@ -51,8 +51,6 @@ write.csv(soc, "socdat.csv", row.names = F)
 # render map
 w <- leaflet() %>% 
   addProviderTiles(providers$OpenStreetMap.Mapnik) %>%
-  addCircleMarkers(prof$Lon, prof$Lat, clusterOptions = markerClusterOptions())
+  addCircleMarkers(prof$lon, prof$lat, clusterOptions = markerClusterOptions())
 w ## plot widget 
-
-# save widget
-saveWidget(w, 'SOC_profile.html', selfcontained = T)
+saveWidget(w, 'SOC_locs.html', selfcontained = T)
